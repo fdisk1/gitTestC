e6807d3f6560cba8655036326a85ca0235b2b987
chenchanglee <92417020+chenchanglee@users.noreply.github.com>
2023-10-27 18:28:41 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-podman run --interactive --rm quay.io/coreos/butane:release --pretty --strict < m1.fcc > container.ign
-sudo coreos-installer install /dev/sda --ignition-file container.ign --copy-network  --insecure-ignition
+podman run --interactive --rm quay.io/coreos/butane:release --pretty --strict < 'm1.fcc' > 'container.ign'
+sudo coreos-installer install /dev/sda --ignition-file 'container.ign' --copy-network  --insecure-ignition
